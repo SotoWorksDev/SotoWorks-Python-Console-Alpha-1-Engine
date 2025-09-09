eb36a15266148275ec3b12b0e3bbd01fde2fddec
SotoWorksDev <mariosotostudios@gmail.com>
2025-09-09 02:55:23 -0700

    Add PLAYER_STATS dictionary for player attributes

---
+++ src/player_stats.py
@@ -0,0 +1,11 @@
+PLAYER_STATS = {
+    "health": 100,
+    "health_regen_rate": 25,  # percent, out of combat
+    "armor": {
+        "active": False,
+        "durability": 100  # durability bar for Syndicate Armor
+    },
+    "stealth_meter": 0,  # 0-100
+    "stamina": 100,
+    "credits": 0,
+}
