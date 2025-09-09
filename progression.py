9f6d96bfa74c3f951126d4c230e97f623bd3888f
SotoWorksDev <mariosotostudios@gmail.com>
2025-09-09 02:56:33 -0700

    Add weapon definitions to progression.py

---
+++ progression.py
@@ -0,0 +1,8 @@
+WEAPONS = [
+    {"name": "StarGun", "unlocks": "start", "upgrades_at": "L6"},
+    {"name": "Combat Knife", "unlocks": "start"},
+    {"name": "Rocket Launcher", "unlocks": "L1.5", "ammo": 2},
+    {"name": "EMP Grenades", "unlocks": "mid-campaign"},
+    {"name": "Flamethrower", "unlocks": "NG+", "code": "FIREWALL"},
+    {"name": "Phantom’s Blade", "unlocks": "Phantom QTE"},
+]
