7309369b783993c8219764efcca66c613d49c74a
SotoWorksDev <mariosotostudios@gmail.com>
2025-09-09 21:06:08 -0700

    Integrate Player class into GameScene

--- src/scenes/scene_game.py
+++ src/scenes/scene_game.py
@@ -1,18 +1,21 @@
 import pygame as pg
+from engine.player import Player
 
 class GameScene:
     def __init__(self, screen):
         self.screen = screen
         self.font = pg.font.SysFont("Consolas", 32)
-        self.message = "Game Scene (stub)"
+        self.player = Player(100, 100)
 
     def handle_event(self, event):
-        pass
+        pass  # We’ll use key states in update
 
     def update(self, dt):
-        pass
+        keys = pg.key.get_pressed()
+        self.player.handle_input(keys, dt)
 
     def draw(self):
         self.screen.fill((20, 20, 30))
-        label = self.font.render(self.message, True, (255,255,255))
-        self.screen.blit(label, (100, 100))
+        self.player.draw(self.screen)
+        label = self.font.render("Use WASD or arrow keys to move!", True, (255,255,255))
+        self.screen.blit(label, (50, 30))
